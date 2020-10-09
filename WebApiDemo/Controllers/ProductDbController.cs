78d1c0d998773bde2543abf2e500e0de8d32f920
Ovijeet Sircar <sircar.ovijeet@gmail.com>
2020-10-09 16:23:55 +0530

    Added GET, POST , PUT and DELETE along with Model Validation.

--- WebApiDemo/Controllers/ProductDbController.cs
+++ WebApiDemo/Controllers/ProductDbController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using WebApiDemo.Data;
+using WebApiDemo.Models;
 
 namespace WebApiDemo.Controllers
 {
@@ -26,27 +27,65 @@ namespace WebApiDemo.Controllers
 
         // GET: api/ProductDb/5
         [HttpGet("{id}", Name = "Get")]
-        public string Get(int id)
+        public IActionResult Get(int id)
         {
-            return "value";
+            var data = _dbContext.Products.SingleOrDefault(x => x.ProductId == id);
+            if(data==null)
+            {
+                return NotFound("The given ID does not correspond to any data.");
+            }
+            return Ok(data);
         }
         
         // POST: api/ProductDb
         [HttpPost]
-        public void Post([FromBody]string value)
+        public IActionResult Post([FromBody]Product product)
         {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            _dbContext.Products.Add(product);
+            _dbContext.SaveChanges(true);
+            return StatusCode(StatusCodes.Status201Created);
         }
         
         // PUT: api/ProductDb/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
+        public IActionResult Put(int id, [FromBody]Product product)
         {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            if(id!=product.ProductId)
+            {
+                return BadRequest("The id of the product to be updated has to be the same as the requested ID");
+            }
+            try
+            {
+                _dbContext.Products.Update(product);
+                _dbContext.SaveChanges(true);
+                return StatusCode(StatusCodes.Status200OK);
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
         
         // DELETE: api/ApiWithActions/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public IActionResult Delete(int id)
         {
+            var product = _dbContext.Products.SingleOrDefault(x => x.ProductId == id);
+            if(product==null)
+            {
+                return NotFound("The given ID does not correspond to any data.");
+            }
+            _dbContext.Products.Remove(product);
+            _dbContext.SaveChanges(true);
+            return StatusCode(StatusCodes.Status200OK);
         }
     }
 }
--- WebApiDemo/Models/Product.cs
+++ WebApiDemo/Models/Product.cs
@@ -1,4 +1,6 @@
-﻿namespace WebApiDemo.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApiDemo.Models
 {
     /// <summary>
     /// Model class for a sample Product
@@ -6,7 +8,9 @@
     public class Product
     {
         public int ProductId { get; set; }
+        [Required(ErrorMessage ="The product name is mandatory, duh-uh!"),StringLength(20,ErrorMessage = "Please enter a string which is lesser than 20 characters")]
         public string ProductName { get; set; }
+        [Required(ErrorMessage = "The product price is mandatory, duh-uh!")]
         public string ProductPrice { get; set; }
     }
 }
